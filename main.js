29050e639f282a279593ed40e22840394ce2ef6b
trinhhungfischer <trinhhungfischer@gmail.com>
2022-05-31 10:51:28 +0700

    'fix: Change directory'

--- main.js
+++ main.js
@@ -30,8 +30,6 @@ cc.game.onStart = function () {
         //socket
         gv.gameClient = new GameClient();
         gv.poolObjects = new PoolObject();
-        //modules
-        // testnetwork.connector = new testnetwork.Connector(gv.gameClient);
 
         fr.view(ScreenMenu);
     }, this);
