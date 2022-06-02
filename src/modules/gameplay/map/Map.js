0d066ef6348a6508223e2cbf3a2f20c72f37a28e
trinhhungfischer <trinhhungfischer@gmail.com>
2022-06-02 09:06:12 +0700

    'feat: Add cell id'

--- src/modules/gameplay/map/Map.js
+++ src/modules/gameplay/map/Map.js
@@ -9,6 +9,7 @@ var Map = cc.Sprite.extend({
     _checkCell: [],
     _isVisitedCell: [],
     _path: [],
+    _listPath:[],
 
 
     ctor:function() {
@@ -20,8 +21,8 @@ var Map = cc.Sprite.extend({
 
     _init:function () {
         this._randomMapCell();
-        this.startPoint = 0 + (MW.MAP_SIZE_HEIGHT - 1) * MW.MAP_SIZE_WIDTH;
-        this.endPoint = (MW.MAP_SIZE_WIDTH - 1) + MW.MAP_SIZE_HEIGHT * 0;
+        this.startPoint = this._getCellIdFromPos(0, MW.MAP_SIZE_HEIGHT - 1);
+        this.endPoint = this._getCellIdFromPos(MW.MAP_SIZE_WIDTH - 1, 0);
 
 
         for (var i = 0; i < MW.MAP_SIZE_WIDTH; i++) {
@@ -61,20 +62,25 @@ var Map = cc.Sprite.extend({
         Hàm sẽ di chuyển từ vị trí góc trên bên trái tới góc dưới bên phải màn hình
         hay nghĩa là đi từ vị trí (0, 6) tới vị trí (6, 0)
      */
-    _pathFinder: function (){
-        while (this._path.length != 0) {
+    _pathFinder: function (startCellId, targetCellId){
 
-        }
     },
 
     _getNextCell: function (cellId) {
         var __nextCell = [];
 
-        var [__xPos, __yPos] = this._getPosFromCellID(cellId);
+        var [__xPos, __yPos] = this._getPosFromCellId(cellId);
 
+        var listCell = [[__xPos + 1, __yPos], [__xPos - 1, __yPos], [__xPos, __yPos + 1], [__xPos, __yPos - 1]];
 
+        for (var i = 0; i < listCell.length; i ++) {
+            var [__nextCellXPos, __nextCellYPos] = listCell[i];
 
+            if (this._isLegalCell(__nextCellXPos, __nextCellYPos))
+                __nextCell.push(this._getCellIdFromPos(__nextCellXPos, __nextCellYPos));
+        };
 
+        return __nextCell;
     },
 
     _isLegalCell: function (xPos, yPos) {
@@ -82,10 +88,13 @@ var Map = cc.Sprite.extend({
             (yPos >= 0) && (yPos < MW.MAP_SIZE_WIDTH))
     },
 
-    _getPosFromCellID: function (cellId) {
+    _getPosFromCellId: function (cellId) {
         return [cellId % MW.MAP_SIZE_WIDTH, Math.floor(cellId / MW.MAP_SIZE_WIDTH)];
-    }
+    },
 
+    _getCellIdFromPos:function (xPos, yPos) {
+        return xPos + yPos * MW.MAP_SIZE_WIDTH;
+    }
 
 
 });
